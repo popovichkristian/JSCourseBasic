d62460ba5a8e9f56ebfd74f99ef9fb12a33d56c7
popovichkristian <popovichkristian@gmail.com>
2022-08-08 16:07:25 +0300

    2. Mutation in JavaScript

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //1. Переменные - Variables (Переменные - это сущности в которых хранятся значения)
 
-var name = 'Kristian' // VAR - устаревший тип переменных. Сейчас лучше обходится только let, const.
-const lastName = 'Popovich' // тип данных - string / Если в последствии не планируется изменять значение переменной - то лучше использовать const.
+// var name = 'Kristian' // VAR - устаревший тип переменных. Сейчас лучше обходится только let, const.
+// const lastName = 'Popovich' // тип данных - string / Если в последствии не планируется изменять значение переменной - то лучше использовать const.
+const firstName = 'Kristian'
 let age = 26 // тип данных - number
 const isProgrammer = true // тип данных - boolean
 
@@ -15,4 +16,10 @@ console.log(age)
 console.log(isProgrammer)
 
 // 2. Мутирование
-console.log('')
+console.log('Имя человека: ' + name + ', а возраст человека: ' + age)
+console.log(age.toString())
+// alert('Имя человека: ' + name + ', а возраст человека: ' + age)
+// prompt('Ведите фамилию')
+
+const lastName = prompt('Введите фамилию:')
+alert(firstName + ' ' + lastName)
