13cf68f40b26d6e4c5248fd215f91aee82ca6eb0
popovichkristian <popovichkristian@gmail.com>
2022-08-08 16:22:15 +0300

    Операторы JavaScript

--- app.js
+++ app.js
@@ -3,23 +3,47 @@
 // var name = 'Kristian' // VAR - устаревший тип переменных. Сейчас лучше обходится только let, const.
 // const lastName = 'Popovich' // тип данных - string / Если в последствии не планируется изменять значение переменной - то лучше использовать const.
 const firstName = 'Kristian'
-let age = 26 // тип данных - number
+// let age = 26 // тип данных - number
 const isProgrammer = true // тип данных - boolean
-
 // lastName = 'Ion' //переменные которые находятся в CONST - в последствии невозможно переопределять (менять)...
 
-name = 'Ivan' //переменные которые находятся в VAR - в последствии можно переопределять (менять)...
-age = 28 //переменные которые находятся в LET - в последствии тоже можно переопределять (менять)...
+// name = 'Ivan' //переменные которые находятся в VAR - в последствии можно переопределять (менять)...
+// age = 28 //переменные которые находятся в LET - в последствии тоже можно переопределять (менять)...
 
-console.log(name)
-console.log(age)
-console.log(isProgrammer)
+// console.log(name)
+// console.log(age)
+// console.log(isProgrammer)
 
 // 2. Мутирование
-console.log('Имя человека: ' + name + ', а возраст человека: ' + age)
-console.log(age.toString())
+// console.log('Имя человека: ' + name + ', а возраст человека: ' + age)
+// console.log(age.toString())
 // alert('Имя человека: ' + name + ', а возраст человека: ' + age)
 // prompt('Ведите фамилию')
 
-const lastName = prompt('Введите фамилию:')
-alert(firstName + ' ' + lastName)
+// const lastName = prompt('Введите фамилию:')
+// alert(firstName + ' ' + lastName)
+
+// 3 Опереторы
+let currentYear = 2022
+const birthYear = 1998
+// const age = currentYear - birthYear
+// console.log(age)
+
+const a = 10
+const b = 5
+
+let c = 32
+// // c = c + a
+// c += a // c = c + a
+// c -= a // c = c - a
+// c *= a // c = c * a
+// c /= a // c = c / a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a / b)
+// console.log(a * b)
+// console.log (currentYear++)
+// console.log (--currentYear)
+// console.log (c)
+
