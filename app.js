23d9d3e81e442edd93bd791c72c62ead5e0a3e86
popovichkristian <popovichkristian@gmail.com>
2022-08-10 21:39:23 +0300

    freeCodeCamp - Start

---
+++ app.js
@@ -0,0 +1,37 @@
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+var camper = "James";
+var camper = "David";
+// console.log(camper);
+
+let myVar = 1;
+myVar += 5;
+console.log(myVar);
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
