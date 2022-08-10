51d7929a0b7636a2ed2775f73225f52d42b96cd8
popovichkristian <popovichkristian@gmail.com>
2022-08-10 23:41:40 +0300

    Function (return)

--- app.js
+++ app.js
@@ -1,100 +1,124 @@
-// Only change code below this line
-var a = 5;
-var b = 10;
-var c = "I am a";
-// Only change code above this line
+// // Only change code below this line
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+// // Only change code above this line
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
 
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
 
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
 
 
-var camper = "James";
-var camper = "David";
-// console.log(camper);
+// var camper = "James";
+// var camper = "David";
+// // console.log(camper);
 
-let myVar = 1;
-myVar += 5;
-console.log(myVar);
+// let myVar = 1;
+// myVar += 5;
+// console.log(myVar);
 
-// let a = 3;
-// let b = 17;
-// let c = 12;
+// // let a = 3;
+// // let b = 17;
+// // let c = 12;
 
-// Only change code below this line
-a += 12;
-b += 9;
-c += 7;
+// // Only change code below this line
+// a += 12;
+// b += 9;
+// c += 7;
 
-const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
-console.log(myStr)
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+// console.log(myStr)
 
-// Setup
-let firstLetterOfLastName = "";
-const lastName = "Lovelace";
+// // Setup
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
 
-// Only change code below this line
-firstLetterOfLastName = lastName[0]; // Change this line
+// // Only change code below this line
+// firstLetterOfLastName = lastName[0]; // Change this line
 
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
 
-// Only change code below this line
-const wordBlanks = "Our " + myNoun + " is " + myAdjective + ", and he " + myVerb + " so " + myAdverb; // Change this line
-// Only change code above this line
+// // Only change code below this line
+// const wordBlanks = "Our " + myNoun + " is " + myAdjective + ", and he " + myVerb + " so " + myAdverb; // Change this line
+// // Only change code above this line
 
-const myArray = [50, 60, 70];
-const myData = myArray[0]
+// const myArray = [50, 60, 70];
+// const myData = myArray[0]
 
-// Modify Array Data With Indexes
-const myArray = [18, 64, 99];
-myArray[0] = 45;
+// // Modify Array Data With Indexes
+// // const myArray = [18, 64, 99];
+// myArray[0] = 45;
 
-const myArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14],
-  ];
+// const myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14],
+//   ];
   
-  const myData = myArray[2][1]; //Access Multi-Dimensional Arrays With Indexes  
+//   const myData = myArray[2][1]; //Access Multi-Dimensional Arrays With Indexes  
 
-  // Manipulate Arrays With push()
-const myArray = [["John", 23], ["cat", 2]];
-myArray.push(["dog", 3])
+//   // Manipulate Arrays With push()
+// const myArray = [["John", 23], ["cat", 2]];
+// myArray.push(["dog", 3])
 
-// Manipulate Arrays With pop() | .pop() removes the last element from an array and returns that element.
-const myArray = [["John", 23], ["cat", 2]];
-const removedFromMyArray = myArray.pop()
+// // Manipulate Arrays With pop() | .pop() removes the last element from an array and returns that element.
+// const myArray = [["John", 23], ["cat", 2]];
+// const removedFromMyArray = myArray.pop()
 
-// Manipulate Arrays With shift() | That's where .shift() comes in. It works just like .pop(), except it removes the first element instead of the last.
-const myArray = [["John", 23], ["dog", 3]];
-const removedFromMyArray = myArray.shift()
+// // Manipulate Arrays With shift() | That's where .shift() comes in. It works just like .pop(), except it removes the first element instead of the last.
+// const myArray = [["John", 23], ["dog", 3]];
+// const removedFromMyArray = myArray.shift()
 
-// Manipulate Arrays With unshift() | .unshift() works exactly like .push(), but instead of adding the element at the end of the array, unshift() adds the element at the beginning of the array.
-const myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-myArray.unshift(["Paul", 35]);
+// // Manipulate Arrays With unshift() | .unshift() works exactly like .push(), but instead of adding the element at the end of the array, unshift() adds the element at the beginning of the array.
+// const myArray = [["John", 23], ["dog", 3]];
+// myArray.shift();
+// myArray.unshift(["Paul", 35]);
 
-// Global vs. Local Scope in Functions
-const outerWear = "T-Shirt";
+// // Global vs. Local Scope in Functions
+// const outerWear = "T-Shirt";
 
-function myOutfit() {
-  // Only change code below this line
-  const outerWear = "sweater"
-  // Only change code above this line
-  return outerWear;
-}
-myOutfit();
+// function myOutfit() {
+//   // Only change code below this line
+//   const outerWear = "sweater"
+//   // Only change code above this line
+//   return outerWear;
+// }
+// myOutfit();
+
+//  // Undefined Value returned from a Function
+// let sum = 0;
+
+// function addThree() {
+//   sum = sum + 3;
+// }
+
+// // Only change code below this line
+// function addFive() {
+//   sum += 5;
+// }
+
+// // Only change code above this line
+
+// addThree();
+// addFive();
+
+function processArg(num) {
+    return (num + 3) / 5;
+  }
+  processArg(7)
+
+  console.log(processArg)
