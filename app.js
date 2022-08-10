2904887060e0bf4ce3c891248784f9a5bb6ff12f
popovichkristian <popovichkristian@gmail.com>
2022-08-10 23:28:44 +0300

    Global vs. Local Scope in Functions

--- app.js
+++ app.js
@@ -88,3 +88,13 @@ const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
 myArray.unshift(["Paul", 35]);
 
+// Global vs. Local Scope in Functions
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater"
+  // Only change code above this line
+  return outerWear;
+}
+myOutfit();
